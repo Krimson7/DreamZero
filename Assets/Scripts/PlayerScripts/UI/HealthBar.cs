63792a855e863fb4faadf63764c0c51c29ccfc77
Krimson777 <keenno2001@gmail.com>
2022-06-16 22:09:53 +0700

    trash

--- Assets/Scripts/PlayerScripts/UI/HealthBar.cs
+++ Assets/Scripts/PlayerScripts/UI/HealthBar.cs
@@ -6,17 +6,13 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
-    public Gradient gradient;
-    public Image fill;
 
     public void setMaxHealth(float max){
         slider.maxValue = max;
-        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     public void setHealth(float health){
         slider.value = health;
-        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
     // Start is called before the first frame update
     void Start()
