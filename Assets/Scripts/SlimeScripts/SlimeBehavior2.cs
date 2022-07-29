fe4ade81d552b3aabfb58cf693555e523da3802d
Krimson777 <keenno2001@gmail.com>
2022-07-29 12:59:46 +0700

    +Slime attacking behaviors
    Behavior1 = Wadle dee passive walking
    Bahavior2 = alert and charge at player when in range

--- Assets/Scripts/SlimeScripts/SlimeBehavior2.cs
+++ Assets/Scripts/SlimeScripts/SlimeBehavior2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeBehavior1 : MonoBehaviour
+public class SlimeBehavior2 : MonoBehaviour
 {
     public Animator animator;
     private enum State{
@@ -18,11 +18,11 @@ public class SlimeBehavior1 : MonoBehaviour
 
     public int facingRight = -1;
     public Rigidbody2D rb;
-    public float walkSpeed;
+    public float walkSpeed = 60f;
     public bool needsFlip;
     public float atk = 10f;
-    public float atkMaxSpeed = 1f;
-    public float atkRampRate = 0.1f;
+    public float atkMaxSpeed = 300f;
+    public float atkRampRate = 1f;
     public float atkSpeed = 0f;
     public bool isAttacking = false;
     public float atkDelay = 1f;
@@ -46,6 +46,11 @@ public class SlimeBehavior1 : MonoBehaviour
     public bool playerDetected;
     public bool playerOnRight;
 
+    public Collider2D playerCharged;
+    public Transform chargeHitPoint;
+    public Vector2 chargeHitSize;
+    public bool playerInChargeZone = false;
+
     public float gravity = -1f;
 
     public float idleTimer;   // Time until quit idling
@@ -67,17 +72,22 @@ public class SlimeBehavior1 : MonoBehaviour
 
         playerInRange = Physics2D.OverlapCircle(transform.position, playerDetectionRange, playerLayer);
         if(playerInRange?.transform.position.y >= transform.position.y-0.3f){
-            playerOnRight = playerInRange.transform.position.x >= transform.position.x - facingRight*0.3f;
+            playerOnRight = playerInRange.transform.position.x >= transform.position.x - facingRight * 0.3f;
             playerDetected = (!playerOnRight ^ facingRight==1)? true:false;
-
         }else{
             playerDetected = false;
         }
-        
+        playerCharged = Physics2D.OverlapBox(chargeHitPoint.position, chargeHitSize, 0f, playerLayer);
+        playerInChargeZone = playerCharged? true:false;
 
         switch(state){
             case State.Idle:
-                if(playerDetected && atkStage == attackingStage.idle){
+                // if(prevState == State.Attacking){
+                //     Idle();
+                //     idleCounter = idleTimer - atkDelay;
+                //     StartCoroutine(AttackDelay());
+                // }else 
+                if(playerDetected){
                     changeState(State.Attacking);
                 }else if(idleCounter<=idleTimer){
                     Idle();
@@ -91,14 +101,12 @@ public class SlimeBehavior1 : MonoBehaviour
                 break;
             case State.Wander:
                 Wander();
-                if(playerDetected){
-                    changeState(State.Attacking);
-                }
+                if(playerDetected) changeState(State.Attacking);
                 break;
             case State.Attacking:
                 if(atkStage==attackingStage.idle) atkStage = attackingStage.initiate;
                 Attack();
-                if((!playerDetected || (playerOnRight ^ facingRight==1)) && !isAttacking){
+                if((!playerDetected || (playerOnRight ^ facingRight==1)) && !isAttacking){  //&& !isAttacking){
                     // Debug.Log("player not seen");
                     atkStage = attackingStage.idle;
                     changeState(State.Idle);
@@ -129,37 +137,46 @@ public class SlimeBehavior1 : MonoBehaviour
         }
     }
     public void Attack(){
-        switch(atkStage){
-            default: 
-                break;
-            case attackingStage.initiate:
-                StartCoroutine(InitAttack());
-                break;
-            case attackingStage.running:
+        
+        if(atkStage == attackingStage.initiate){
+            StartCoroutine(InitRushAttack());
+        }else if(atkStage == attackingStage.running){
+            animator.Play("Slime_Run");
+            if(playerCharged){
+                playerCharged.gameObject.GetComponent<playerStateMachine>().checkTakeDamage(atk);
+            }
+            if(playerDetected && (!playerOnRight ^ facingRight==1)){
+                rb.velocity = new Vector2(facingRight * atkSpeed * Time.fixedDeltaTime, rb.velocity.y); 
+                if(atkSpeed <= atkMaxSpeed){
+                    atkSpeed += atkRampRate;
+                }else if(atkSpeed > atkMaxSpeed){
+                    atkSpeed = atkMaxSpeed;
+                }
+            }else{
+                rb.velocity = new Vector2(facingRight * atkSpeed * Time.fixedDeltaTime, rb.velocity.y); 
                 if(!isAttacking)
                     StartCoroutine(AttackDelay());
-                break;
+            }
+            
         }
     }
 
-    IEnumerator InitAttack(){
+    IEnumerator InitRushAttack(){
         animator.Play("Slime_Landing");
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.3f);
+        // isAttacking = true;
         atkStage = attackingStage.running;
+
     }
 
     IEnumerator AttackDelay(){
-        animator.Play("Slime_Jump");
         isAttacking = true;
-        playerInRange?.GetComponent<playerStateMachine>().checkTakeDamage(atk);
-        // atkStage = attackingStage.cooldown;
         yield return new WaitForSeconds(atkDelay);
-        Debug.Log("waitingDone?");
+        atkSpeed = 0f;
         isAttacking = false;
         atkStage = attackingStage.idle;
         prevState = State.Idle;
         changeState(State.Idle);
-        
     }
 
     public void Flip(){
@@ -171,10 +188,10 @@ public class SlimeBehavior1 : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere((Vector3)groundCheck.transform.position, 0.1f);
         Gizmos.DrawWireSphere((Vector3)wallCheck.transform.position, 0.1f);
-        if(playerDetected){
-            Gizmos.color = Color.green;
-        }
+        if(playerDetected) Gizmos.color = Color.green;
         Gizmos.DrawWireSphere((Vector3)transform.position, playerDetectionRange);
+        Gizmos.color = playerCharged? Color.green : Color.red;
+        Gizmos.DrawWireCube((Vector3)chargeHitPoint.position, chargeHitSize);
     }
 
     void changeState(State nextState){
